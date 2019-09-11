44776ca52a7cd1f06f3bfddf3ab575ea9ab0e688
Fábio Sales <fabio.oliveira.sales@gmail.com>
2019-09-10 22:15:58 -0300

    criando toolbar e recyclerview.

--- app/src/main/java/br/com/nybooks/presentation/books/BooksActivity.kt
+++ app/src/main/java/br/com/nybooks/presentation/books/BooksActivity.kt
@@ -3,11 +3,15 @@ package br.com.nybooks.presentation.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import br.com.nybooks.R
+import kotlinx.android.synthetic.main.activity_books.*
 
 class BooksActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_books)
+
+        toobarMain.title ="Books"
+        setSupportActionBar(toobarMain)
     }
 }
